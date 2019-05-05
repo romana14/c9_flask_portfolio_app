cc4c644d293a13c43da9cdaf156c4ce2b769f47f
romana14 <49789984+romana14@users.noreply.github.com>
2019-05-05 09:50:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = name + " likes eating bananas"
+	new_name = str(name) + " likes eating bananas"
 	return render_template('index.html', name=new_name)
 
 
